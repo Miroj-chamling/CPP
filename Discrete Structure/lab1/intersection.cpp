b0a634ebe116e8bd46f95f617d2960cb3c81287d
Miroj <zorim.rai@gmail.com>
2022-01-16 08:52:39 +0545

    intersection

--- Discrete Structure/lab1/intersection.cpp
+++ Discrete Structure/lab1/intersection.cpp
@@ -3,6 +3,32 @@ using namespace std;
 
 int main()
 {
-    cout << "hellop world";
+    int A[10], B[10], I[10];
+    int n, m, j, i;
+    cout << "enter the size of the first set: ";
+    cin >> n;
+    cout << "Enter the elements of the first set: ";
+    for (i = 0; i < n; i++)
+    {
+        cin >> A[i];
+    }
+    cout << "enter the size of the second set: ";
+    cin >> m;
+    cout << "enter the elements of the second set: ";
+    for (j = 0; j < m; j++)
+    {
+        cin >> B[j];
+    }
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < m; j++)
+        {
+            if (A[i] == B[j])
+            {
+                cout << A[i];
+            }
+        }
+    }
+
     return 0;
 }
